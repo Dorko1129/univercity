a75212cf5dabc24957e874be44db52a0115d6c85
Dorko1129 <161713914+Dorko1129@users.noreply.github.com>
2024-03-03 09:28:05 +0100

    Add files via upload

---
+++ pozitiv.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+int szam;
+int k=0;
+printf("0 = vege  ");
+do
+{
+    printf("adjon meg egy szamot: ");
+    scanf("%d", &szam);
+    printf("bekert szam: %d\n",szam);
+    
+    if (szam>0)
+    {
+        k+=1;
+    }
+} while (szam>0);
+
+    printf("pozitiv szamok darabja: %d\n", k);
+
+
+
+
+    return 0;
+}
