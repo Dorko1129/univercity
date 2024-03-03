30d2043b45f1df2d5a837c40c8493348c459e870
Dorko1129 <161713914+Dorko1129@users.noreply.github.com>
2024-03-03 09:18:02 +0100

    Add files via upload

---
+++ vege0.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+int szam;
+int k=0;
+printf("ha 0-t ir be, akkor a program leall!  ");
+do
+{
+    printf("adjon meg egy szamot: ");
+    scanf("%d", &szam);
+    printf("bekert szam: %d\n",szam);
+    
+    k+=szam;
+} while (szam>0);
+if (k>0)
+{
+    printf("az osszeg: %d\n", k);
+}
+
+
+
+
+    return 0;
+}
